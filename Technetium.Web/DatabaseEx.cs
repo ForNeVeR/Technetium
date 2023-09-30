15f7f6c0cbb1be6cec0fe54e87bc2c398641e4a3
Friedrich von Never <friedrich@fornever.me>
2023-09-30 19:55:21 +0200

    (#1) Database: initialize and migrate together with the app

---
+++ Technetium.Web/DatabaseEx.cs
@@ -0,0 +1,19 @@
+﻿using Technetium.Data;
+
+namespace Technetium.Web;
+
+internal static class DatabaseEx
+{
+    public static async Task MigrateDatabase(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+        logger.LogInformation("Migrating database.");
+
+        var context = scope.ServiceProvider.GetRequiredService<TechnetiumDataContext>();
+        await context.Migrate();
+
+        logger.LogInformation("Database migration done.");
+    }
+}
--- Technetium.Web/Program.cs
+++ Technetium.Web/Program.cs
@@ -1,5 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Technetium.Data;
+using Technetium.Web;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddDbContext<TechnetiumDataContext>(opts => opts.UseSqlite(builder.Configuration.GetConnectionString("Database")));
+
 var app = builder.Build();
+await app.MigrateDatabase();
 
 app.MapGet("/", () => "Hello World!");
 
