f8a202366f858f756d1832ba673d1caa4862c4a8
Friedrich von Never <friedrich@fornever.me>
2023-10-14 16:40:24 +0300

    (#1) Frontend: add a simple React app

---
+++ Technetium.Frontend/app.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+const App = () => {
+    return <>Hello, Technetium!</>;
+};
+
+render(<App/>, document.getElementById('app'));
--- Technetium.Web/Program.cs
+++ Technetium.Web/Program.cs
@@ -1,3 +1,6 @@
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
 using Technetium.Data.Extensions;
 using Technetium.Web.Extensions;
 
@@ -5,11 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
 builder.Services.AddDatabase(configuration);
+builder.Environment.WebRootPath = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location)!, "wwwroot");
+builder.Environment.WebRootFileProvider = new PhysicalFileProvider(builder.Environment.WebRootPath);
 
 var application = builder.Build();
 
 await application.ApplyMigrationsAsync();
 
-application.MapGet("/", () => "Hello World!");
+application.UseStaticFiles();
+application.MapGet("/", () => Results.LocalRedirect("/index.html"));
 
 application.Run();
