e134b6d404922786bbc1cee6a85215243f3e4838
Friedrich von Never <friedrich@fornever.me>
2024-01-18 21:39:27 +0100

    (#3) Frontend: use react-big-calendar

--- Technetium.Frontend/Application/ApplicationView.tsx
+++ Technetium.Frontend/Application/ApplicationView.tsx
@@ -1,9 +1,11 @@
 import {ApplicationViewModel} from "./ApplicationViewModel.js";
 import {StatusBarView} from "./StatusBarView.js";
 import React from "react";
+import {CalendarView} from "../Calendar/CalendarView.js";
 
 export const ApplicationView = ({ viewModel }: { viewModel: ApplicationViewModel }) => {
     return [
+        <CalendarView />,
         <StatusBarView status={viewModel.status} />
     ]
 };
---
+++ Technetium.Frontend/Calendar/CalendarView.tsx
@@ -0,0 +1,17 @@
+import {Calendar, momentLocalizer} from 'react-big-calendar';
+import moment from 'moment';
+import React from "react";
+
+const localizer = momentLocalizer(moment);
+
+export const CalendarView = () => (
+    <div>
+        <Calendar
+            localizer={localizer}
+            events={[/* TODO: Pass the events here */]}
+            startAccessor="start"
+            endAccessor="end"
+            style={{ height: 500 }}
+        />
+    </div>
+);
